b9e23be8445ef2fc4ce2e038bf34464ebbec932b
Luiz Mello <luizmacmillan@gmail.com>
2020-11-09 19:34:18 -0300

    CSS Atualization.

--- src/NextPokemonButton.js
+++ src/NextPokemonButton.js
@@ -3,7 +3,14 @@ import React from 'react';
 class NextPokemonButton extends React.Component {
   render() {
     const { currentPokemonHandler } = this.props;
-    return <button onClick={currentPokemonHandler}>Next Pokemon</button>
+    return (
+      <button
+        onClick={currentPokemonHandler}
+        className="next-pokemon-btn"
+      >
+        Next Pokemon
+      </button>
+    );
   }
 }
 
